2c422d1c0a7e39e78d055f124e3f3702134e9c17
Victor <59689369+Geemullo@users.noreply.github.com>
2024-04-22 18:29:54 -0300

    update: add file scriptjs

---
+++ script.js
@@ -0,0 +1,15 @@
+// seleção de elementos
+const toggle = document.querySelector('.toggle');
+
+
+
+
+// funções
+
+
+
+// eventos
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('on');
+});
